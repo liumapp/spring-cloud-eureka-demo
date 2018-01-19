7afee948be6e65f5a55b390d16a5ae5fb916cc5f
liumapp <liumapp.com@gmail.com>
2018-01-19 16:54:21 +0800

    solved

--- eureka-demo-client/src/main/java/com/liumapp/demo/eureka/client/controller/IndexController.java
+++ eureka-demo-client/src/main/java/com/liumapp/demo/eureka/client/controller/IndexController.java
@@ -1,7 +1,8 @@
 package com.liumapp.demo.eureka.client.controller;
 
-import com.netflix.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-//    @Autowired
-//    private DiscoveryClient client;
+    @Autowired
+    private DiscoveryClient client;
 
     @RequestMapping("/")
     public String index () {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        System.out.println("host is : " + instance.getHost() + " and service_id is : " + instance.getServiceId());
         return "Hello World";
     }
 
