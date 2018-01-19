90dcf14a829fe17539de714c5b0ae222e04abb78
liumapp <liumapp.com@gmail.com>
2018-01-19 14:46:23 +0800

    update simple server

--- eureka-demo-server/src/main/java/com/liumapp/demo/eureka/server/Main.java
+++ eureka-demo-server/src/main/java/com/liumapp/demo/eureka/server/Main.java
@@ -1,8 +1,7 @@
-package com.liumapp.demo.eureka;
+package com.liumapp.demo.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
