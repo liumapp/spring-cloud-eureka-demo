df473df23c0eeca942399f84c4dd0c95c27bcdff
liumapp <liumapp.com@gmail.com>
2018-01-19 14:24:38 +0800

    eureka server done

--- src/main/java/com/liumapp/demo/eureka/Main.java
+++ src/main/java/com/liumapp/demo/eureka/Main.java
@@ -2,13 +2,16 @@ package com.liumapp.demo.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Created by liumapp on 9/28/17.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
+@EnableEurekaServer
+@SpringBootApplication()
 public class Main {
 
     public static void main(String[] args) {
