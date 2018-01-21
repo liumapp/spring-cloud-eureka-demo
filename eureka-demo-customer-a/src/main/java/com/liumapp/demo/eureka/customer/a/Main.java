bb3daa098f2a5e6c34816c32e75fb1265eb4080f
liumapp <liumapp.com@gmail.com>
2018-01-21 13:09:29 +0800

    add customer controller

--- eureka-demo-customer-a/src/main/java/com/liumapp/demo/eureka/customer/a/Main.java
+++ eureka-demo-customer-a/src/main/java/com/liumapp/demo/eureka/customer/a/Main.java
@@ -3,6 +3,9 @@ package com.liumapp.demo.eureka.customer.a;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by liumapp on 9/28/17.
@@ -12,6 +15,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.liumapp.demo.eureka.customer.a"})
 public class Main {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate () {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class , args);
