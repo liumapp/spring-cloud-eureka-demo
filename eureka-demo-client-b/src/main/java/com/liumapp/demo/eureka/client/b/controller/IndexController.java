59317910ef1063cd4e7792811a7738f5e8a44315
liumapp <liumapp.com@gmail.com>
2018-01-21 12:34:12 +0800

    client b done

--- eureka-demo-client-b/src/main/java/com/liumapp/demo/eureka/client/b/controller/IndexController.java
+++ eureka-demo-client-b/src/main/java/com/liumapp/demo/eureka/client/b/controller/IndexController.java
@@ -1,10 +1,7 @@
 package com.liumapp.demo.eureka.client.b.controller;
 
-import com.liumapp.demo.eureka.client.b.entity.Guest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -16,15 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class IndexController {
 
-    @Autowired
-    private Guest guest;
-
-    @GetMapping("/")
+    @RequestMapping("/")
     public String index (ModelMap model) {
-
-        model.addAttribute("name" , guest.getName());
-        model.addAttribute("sex" , guest.getSex());
-        return "index";
+        return "hello , this is client b demo";
     }
 
 }
